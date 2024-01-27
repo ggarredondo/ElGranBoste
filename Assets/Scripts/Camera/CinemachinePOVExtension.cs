fbdc3ba51cac02b11954776b409bb44c88b1811d
Alejandro Cruz Lemos <alexcruzlemos12082000@gmail.com>
2024-01-27 11:46:49 +0100

    Player FootSteps sound

--- Assets/Scripts/Camera/CinemachinePOVExtension.cs
+++ Assets/Scripts/Camera/CinemachinePOVExtension.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using Cinemachine;
 using UnityEngine.InputSystem;
 using DG.Tweening;
+using System.Collections.Generic;
 
 public class CinemachinePOVExtension : CinemachineExtension
 {
@@ -22,6 +23,9 @@ public class CinemachinePOVExtension : CinemachineExtension
     [SerializeField] private float amplitudeX = 0.1f;
     [SerializeField] private float shakeFrequency = 0.5f;
 
+    [Header("Sounds")]
+    [SerializeField] private string stepSoundName;
+
     private Vector2 mouseInput;
     private Vector3 startingRotation;
     private Transform target;
@@ -33,6 +37,8 @@ public class CinemachinePOVExtension : CinemachineExtension
     {
         look.action.performed += ctx => mouseInput = ctx.ReadValue<Vector2>();
         target = VirtualCamera.Follow;
+
+        
 
         sequenceY = DOTween.Sequence();
         sequenceX = DOTween.Sequence();
@@ -54,8 +60,10 @@ public class CinemachinePOVExtension : CinemachineExtension
 
     private void UpdateDuration()
     {
-        sequenceX.DOTimeScale(player.Velocity.magnitude / 5, 0f);
-        sequenceY.DOTimeScale(player.Velocity.magnitude / 5, 0f);
+        float velocity = player.Velocity.magnitude / player.RunningState.MovementSpeed;
+
+        sequenceX.DOTimeScale(velocity, 0f);
+        sequenceY.DOTimeScale(velocity, 0f);
     }
 
     private void CameraShake()
@@ -64,7 +72,12 @@ public class CinemachinePOVExtension : CinemachineExtension
         sequenceY.SetLoops(-1, LoopType.Yoyo);
 
         sequenceX.Append(target.DOLocalMoveX(amplitudeX, shakeFrequency * 2).From(-amplitudeX).SetEase(Ease.InOutSine));
-        sequenceX.SetLoops(-1, LoopType.Yoyo);
+        sequenceX.SetLoops(-1, LoopType.Yoyo).OnStepComplete(PlaySound); ;
+    }
+
+    private void PlaySound()
+    {
+        GameManager.Audio.Play(stepSoundName);
     }
 
     protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
