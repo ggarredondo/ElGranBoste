b01e0dde0d3dd4e177c0bf7a7ef8b572fe31f07e
Alejandro Cruz Lemos <alexcruzlemos12082000@gmail.com>
2024-01-27 23:26:23 +0100

    Minor Changes

--- Assets/Scripts/JokeBook/JokeBook.cs
+++ Assets/Scripts/JokeBook/JokeBook.cs
@@ -1,10 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.WSA;
-using UnityEngine.InputSystem;
 using DG.Tweening;
-using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class JokeBook : MonoBehaviour
 {
