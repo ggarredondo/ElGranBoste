454efd79054ff405719ccc2b004cc94b68e345b1
Guillermo García <55685909+ggarredondo@users.noreply.github.com>
2024-01-27 20:45:09 +0100

    Implemented enemy animations

--- Assets/Scripts/Enemy/EnemyAnimationBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyAnimationBehaviour.cs
@@ -8,5 +8,16 @@ public class EnemyAnimationBehaviour : MonoBehaviour
     private void Start()
     {
         enemyStateMachine = GetComponent<EnemyStateMachine>();
+
+        enemyStateMachine.ChasingState.OnEnter += () => animator.SetBool("STATE_CHASING", true);
+        enemyStateMachine.ChasingState.OnExit += () => animator.SetBool("STATE_CHASING", false);
+
+        enemyStateMachine.ListeningState.OnEnter += () => animator.SetBool("STATE_LISTENING", true);
+        enemyStateMachine.ListeningState.OnExit += () => animator.SetBool("STATE_LISTENING", false);
+
+        enemyStateMachine.LaughingState.OnEnter += () => animator.SetTrigger("laugh");
+
+        enemyStateMachine.PosteState.OnEnter += () => animator.SetBool("STATE_POSTE", true);
+        enemyStateMachine.PosteState.OnExit += () => animator.SetBool("STATE_POSTE", false);
     }
 }
