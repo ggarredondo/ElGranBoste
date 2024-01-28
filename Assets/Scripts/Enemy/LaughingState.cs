38c28b3385c7d6358e71d69a1b30622276734725
Guillermo García <55685909+ggarredondo@users.noreply.github.com>
2024-01-28 12:50:16 +0100

    Update LaughingState.cs

--- Assets/Scripts/Enemy/LaughingState.cs
+++ Assets/Scripts/Enemy/LaughingState.cs
@@ -11,6 +11,7 @@ public class LaughingState : EnemyState
 
     public override void Enter()
     {
+        enemy.PlayerToEnemyEvents.OnJokePerformed += ReceiveJoke;
         sequence = DOTween.Sequence();
         sequence.AppendInterval(receivedJoke.LaughingTime).OnComplete(enemy.TransitionToChasing);
         enemy.StopFollowing();
@@ -19,7 +20,15 @@ public class LaughingState : EnemyState
     public override void Update() {}
     public override void Exit()
     {
+        sequence.Kill();
+        enemy.PlayerToEnemyEvents.OnJokePerformed -= ReceiveJoke;
         base.Exit();
+    }
+
+    private void ReceiveJoke(in Joke joke)
+    {
+        if (joke.Type == JokeType.Stun) enemy.TransitionToLaughing(joke);
+        else if (joke.Type == JokeType.Poste) enemy.TransitionToPoste();
     }
 
     public ref readonly Joke Joke => ref receivedJoke;
