fcacf00d3f2d42b1aa8823d4609df70c85b25129
Guillermo García <55685909+ggarredondo@users.noreply.github.com>
2024-02-01 12:28:01 +0100

    Added missing colliders to EntornoFinal

--- Assets/Scripts/Player/ParryState.cs
+++ Assets/Scripts/Player/ParryState.cs
@@ -34,6 +34,7 @@ public class ParryState : PlayerState
     public override void Exit()
     {
         sequence.Kill();
+        player.PlayerToEnemyEvents.OnParry = null;
         player.PlayerToEnemyEvents.OnKillPlayer -= player.TransitionToDead;
         base.Exit();
     }
