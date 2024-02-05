f3ce3a2a84c0a57140e8897e3df5e080a274cbfe
Alejandro Cruz Lemos <alexcruzlemos12082000@gmail.com>
2024-02-05 12:01:23 +0100

    Skip loading scene key

--- Assets/Scripts/Menu/LoadingController.cs
+++ Assets/Scripts/Menu/LoadingController.cs
@@ -2,36 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SceneUtilities;
+using UnityEngine.InputSystem;
+using TMPro;
 using DG.Tweening;
 
 public class LoadingController : MonoBehaviour
 {
-    [SerializeField] private float loadingTime;
+    [Header("Requirements")]
+    [SerializeField] private GameObject continueObject;
+    [SerializeField] private TMP_Text loadText;
+    [SerializeField] private InputActionReference actionReference;
+
+    [Header("Parameters")]
+    [SerializeField] private string continueText;
+    [Space(5)]
+    [SerializeField] private float endScaleAnimation;
+    [SerializeField] private float animationDuration;
 
     private bool isLoaded;
-    private Sequence sequence;
+    private Sequence continueSequence;
+    private RectTransform rectTransform;
 
     private void Start()
     {
         SceneLoader.UpdateLoading += UpdateLoading;
+        actionReference.action.performed += TriggerAction;
 
-        Timer();
+        rectTransform = continueObject.GetComponent<RectTransform>();
+        loadText.text = continueText;
+        continueObject.SetActive(false);
+        InitializeAnimation();
     }
 
     private void OnDestroy()
     {
         SceneLoader.UpdateLoading -= UpdateLoading;
+        actionReference.action.performed -= TriggerAction;
+
+        continueSequence.Kill();
     }
 
-    private void Timer()
+    private void TriggerAction(InputAction.CallbackContext ctx)
     {
-        sequence = DOTween.Sequence();
-        sequence.AppendInterval(loadingTime);
-        sequence.OnComplete(() => isLoaded = true);
+        isLoaded = true;
+    }
+
+    private void InitializeAnimation()
+    {
+        continueSequence = DOTween.Sequence();
+        continueSequence.Append(rectTransform.DOScale(endScaleAnimation, animationDuration));
+        continueSequence.SetLoops(-1, LoopType.Yoyo);
     }
 
     private bool UpdateLoading(float progress)
     {
+        if (progress >= 0.9f)
+        {
+            continueObject.SetActive(true);
+            return isLoaded;
+        }
+
+        isLoaded = false;
         return isLoaded;
     }
 }
